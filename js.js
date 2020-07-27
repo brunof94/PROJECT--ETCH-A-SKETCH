f32735d0146597cba76926dbe9f9c5172ed6da93
brunof94 <bruno10652012@gmail.com>
2020-07-26 21:03:35 -0300

    Update js.js
    Random color function

--- js.js
+++ js.js
@@ -21,7 +21,6 @@ container.appendChild(breakPoint);
 generateGrid(gridLength);
 
 function generateGrid(gridLength) {
-    let x, y, z, colorInString = '';
     pixelPerSquare = 960/gridLength;
 for (i = 0; i <= gridLength; i ++){
     for (j = 0; j <= gridLength; j ++){
@@ -31,12 +30,8 @@ for (i = 0; i <= gridLength; i ++){
         newDiv.style.background = "white";
         newDiv.style.width = pixelPerSquare + "px";
         newDiv.style.height = pixelPerSquare + "px";
-        x = Math.floor(Math.random() * 256);
-        y = Math.floor(Math.random() * 256);
-        z = Math.floor(Math.random() * 256);
-        colorInString = "rgb(" + x + "," + y + "," + z + ")"
         newDiv.addEventListener('mouseover',function(event){
-            event.target.style.background = colorInString;
+            event.target.style.background = randomColor();
         });
         if(counter % (gridLength + 1) != 0){
             newDiv.style.cssFloat= "left";
@@ -47,3 +42,10 @@ for (i = 0; i <= gridLength; i ++){
 }
 }
 
+function randomColor(){
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let colorInString = "rgb(" + x + "," + y + "," + z + ")"
+    return colorInString;
+}
